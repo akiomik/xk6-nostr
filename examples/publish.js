917eef0130e48d1993edecd87df11a87c6da64c6
Akiomi Kamakura <akiomik@gmail.com>
2023-10-18 09:27:03 +0900

    Improve example/publish.js

--- examples/publish.js
+++ examples/publish.js
@@ -1,15 +1,13 @@
 import nostr from 'k6/x/nostr';
 import event from 'k6/x/nostr/event';
 
+const sk = nostr.generatePrivateKey();
+const pk = nostr.getPublicKey(sk);
+const now = Math.round(new Date().getTime() / 1000)
+const ev = event.sign({ content: 'foo', kind: 1, created_at: now }, sk)
+
 export default function () {
-  const sk = nostr.generatePrivateKey();
-  console.log(sk);
-  const pk = nostr.getPublicKey(sk);
-  console.log(pk);
-  const now = Math.round(new Date().getTime() / 1000)
-  const ev = event.sign({ content: 'foo', kind: 1, created_at: now }, sk)
-  console.log(ev);
-  const relay = nostr.relayConnect("wss://relay.damus.io");
+  const relay = nostr.relayConnect("wss://yabu.me");
   const status = relay.publish(ev)
   console.log(status);
 }
