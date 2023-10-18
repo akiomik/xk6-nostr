c485348bd6db98bbb45ed47208587013ba922ee1
Akiomi Kamakura <akiomik@gmail.com>
2023-10-18 18:46:54 +0900

    Remove sleep from examples/publish.js

--- examples/publish.js
+++ examples/publish.js
@@ -1,18 +1,17 @@
 import nostr from 'k6/x/nostr';
 import event from 'k6/x/nostr/event';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 
 const sk = nostr.generatePrivateKey();
 const pk = nostr.getPublicKey(sk);
 const now = Math.round(new Date().getTime() / 1000);
 const ev = event.sign({ content: 'foo', kind: 1, created_at: now }, sk);
-const relay = nostr.relayConnect("ws://localhost:8080");
+const relay = nostr.relayConnect("ws://localhost:7777");
 
 export default function () {
   const status = relay.publish(ev);
   check(status, { 'status is success': (s) => s.string() === 'success' });
-  sleep(1);
 }
 
 export function teardown() {
