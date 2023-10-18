4887ac46f0726771ebf68c8d35a8ffd5bacf0cb6
Akiomi Kamakura <akiomik@gmail.com>
2023-10-18 13:38:40 +0900

    Update exmples/publish.js

--- examples/publish.js
+++ examples/publish.js
@@ -1,5 +1,7 @@
 import nostr from 'k6/x/nostr';
 import event from 'k6/x/nostr/event';
+import { check, sleep } from 'k6';
+
 
 const sk = nostr.generatePrivateKey();
 const pk = nostr.getPublicKey(sk);
@@ -7,7 +9,8 @@ const now = Math.round(new Date().getTime() / 1000)
 const ev = event.sign({ content: 'foo', kind: 1, created_at: now }, sk)
 
 export default function () {
-  const relay = nostr.relayConnect("wss://yabu.me");
+  const relay = nostr.relayConnect("ws://localhost:8080");
   const status = relay.publish(ev)
-  console.log(status);
+  check(status, { 'status is success': (s) => s.string() === 'success' })
+  sleep(1)
 }
