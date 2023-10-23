c476ea69f5ebaeae4f69b560ff2bcf56df0b54d1
Akiomi Kamakura <akiomik@gmail.com>
2023-10-23 10:41:58 +0900

    Update examples/publish-simple.js

--- examples/publish-simple.js
+++ examples/publish-simple.js
@@ -2,12 +2,11 @@ import nostr from 'k6/x/nostr';
 import event from 'k6/x/nostr/event';
 import { check } from 'k6';
 
+const relay = nostr.relayConnect("ws://127.0.0.1:7777");
 
 const sk = nostr.generatePrivateKey();
-const pk = nostr.getPublicKey(sk);
 const now = Math.round(new Date().getTime() / 1000);
 const ev = event.sign({ content: 'foo', kind: 1, created_at: now }, sk);
-const relay = nostr.relayConnect("ws://127.0.0.1:8234");
 
 export default function () {
   const status = relay.publish(ev);
