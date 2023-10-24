f6a452ed47dd30180e1bb03c75053f2b5be93bb4
Akiomi Kamakura <akiomik@gmail.com>
2023-10-25 02:04:37 +0900

    Improve example script

--- examples/publish.js
+++ examples/publish.js
@@ -3,15 +3,12 @@ import event from 'k6/x/nostr/event';
 import { check } from 'k6';
 
 const relay = nostr.relayConnect("ws://127.0.0.1:7777");
-
 const sk = nostr.generatePrivateKey();
-const events = [...Array(50000).keys()].map((i) => {
-  const now = Math.round(new Date().getTime() / 1000);
-  return event.sign({ content: i, kind: 1, created_at: now }, sk);
-});
 
 export default function () {
-  const status = relay.publish(events[__ITER]);
+  const now = Math.round(new Date().getTime() / 1000);
+  const ev = event.sign({ content: Math.random(), kind: 1, created_at: now }, sk);
+  const status = relay.publish(ev);
   check(status, { 'status is success': (s) => s.string() === 'success' });
 }
 
